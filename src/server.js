df615047503e3be8caf2411b82cb58141b426435
Douglas Shinzato <shinzato.douglas@gmail.com>
2024-01-03 17:57:44 -0400

    user update

--- src/server.js
+++ src/server.js
@@ -1,15 +1,17 @@
 require("express-async-errors");
 
+const migrationsRun = require("./database/sqlite/migrations");
 const AppError = require("./utils/AppError");
-
 const express = require("express");
-
 const routes = require("./routes");
-
 const app = express();
+
+migrationsRun();
+
 app.use(express.json());
 
 app.use(routes);
+
 
 app.use((error, request, response, next) => {
   if (error instanceof AppError) {
@@ -19,7 +21,7 @@ app.use((error, request, response, next) => {
     })
   }
 
-  console.log(error)
+  console.error(error)
 
   return response.status(500).json({
     status: 'error',
